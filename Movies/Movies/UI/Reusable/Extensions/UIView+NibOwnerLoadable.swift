7993288939668799ecb75a874a51314f05e77e60
Daniel Astine <daniel@astine.com.br>
2019-08-05 23:04:25 -0300

    Create NibOwnerLoadable extension for UIView

---
+++ Movies/Movies/UI/Reusable/Extensions/UIView+NibOwnerLoadable.swift
@@ -0,0 +1,15 @@
+import class UIKit.UIView
+
+extension NibOwnerLoadable where Self: UIView {
+    func loadNibContent() {
+        for case let view as UIView in Self.nib.instantiate(withOwner: self, options: nil) {
+            addSubview(view)
+            view.layout {
+                $0.top == topAnchor
+                $0.trailing == trailingAnchor
+                $0.bottom == bottomAnchor
+                $0.leading == leadingAnchor
+            }
+        }
+    }
+}
