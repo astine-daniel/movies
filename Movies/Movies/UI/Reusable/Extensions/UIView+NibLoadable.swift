f873e4ec941ad568394f7c250610aae15ba69964
Daniel Astine <daniel@astine.com.br>
2019-08-05 23:02:50 -0300

    Create NibLoadable extension for UIView

---
+++ Movies/Movies/UI/Reusable/Extensions/UIView+NibLoadable.swift
@@ -0,0 +1,11 @@
+import class UIKit.UIView
+
+extension NibLoadable where Self: UIView {
+    static func loadFromNib() -> Self {
+        guard let view = nib.instantiate(withOwner: nil, options: nil).first as? Self else {
+            fatalError("The nib \(nib) expected its root view to be of type \(self)")
+        }
+
+        return view
+    }
+}
