a3066bbfd8f497889a04bfdc9865be37fce87364
Daniel Astine <daniel@astine.com.br>
2019-08-07 15:54:07 -0300

    Create URLSessionProtocol

---
+++ Movies/Movies/Common/Networking/URL/URLSessionProtocol.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+protocol URLSessionProtocol {
+    typealias Handler = (Data?, URLResponse?, Error?) -> Void
+    func dataTask(with request: URLRequest,
+                  completionHandler: @escaping Handler) -> URLSessionDataTask
+}
+
+// MARK: - URLSession extension
+extension URLSession: URLSessionProtocol { }
