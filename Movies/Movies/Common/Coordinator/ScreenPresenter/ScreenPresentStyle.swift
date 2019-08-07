5ee8d4bfc70f8472f0415f959b545e7c47ae3f1c
Daniel Astine <daniel@astine.com.br>
2019-08-07 00:02:01 -0300

    Create Coordinator

---
+++ Movies/Movies/Common/Coordinator/ScreenPresenter/ScreenPresentStyle.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenPresentStyle.swift
+//  Movies
+//
+//  Created by Daniel Astine on 07/08/19.
+//
+
+import Foundation
---
+++ Movies/Movies/Common/Coordinator/ScreenPresenter/ScreenPresenterProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenPresenterProtocol.swift
+//  Movies
+//
+//  Created by Daniel Astine on 07/08/19.
+//
+
+import Foundation
