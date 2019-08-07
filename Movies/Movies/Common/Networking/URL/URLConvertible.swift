61f4d987186082272eb79a975f25dc28be0814e1
Daniel Astine <daniel@astine.com.br>
2019-08-07 15:51:34 -0300

    Create URLConvertible and NetworkingError

---
+++ Movies/Movies/Common/Networking/URL/URLConvertible.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+protocol URLConvertible {
+    func asURL() throws -> URL
+}
+
+// MARK: - String extension
+extension String: URLConvertible {
+    func asURL() throws -> URL {
+        guard let url = URL(string: self) else { throw NetworkingError.invalidURL(url: self) }
+        return url
+    }
+}
+
+// MARK: - URL extension
+extension URL: URLConvertible {
+    func asURL() throws -> URL {
+        return self
+    }
+}
+
+// MARK: - URLComponents
+extension URLComponents: URLConvertible {
+    func asURL() throws -> URL {
+        guard let url = url else { throw NetworkingError.invalidURL(url: self) }
+        return url
+    }
+}
