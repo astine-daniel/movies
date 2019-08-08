d7553f14ec65ad3d468d6a589efe1e9ffd8e66cc
Daniel Astine <daniel@astine.com.br>
2019-08-07 21:46:02 -0300

    Create APIService protocol

---
+++ Movies/Movies/Common/Networking/APIServiceProtocol.swift
@@ -0,0 +1,8 @@
+protocol APIServiceProtocol {
+    typealias Completion<T> = (Result<T, Error>) -> Void
+
+    @discardableResult
+    func request<T>(
+        _ resource: Resource,
+        _ completion: @escaping Completion<T>) -> Request? where T: Decodable
+}
