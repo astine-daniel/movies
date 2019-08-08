6f03f0f000ae3d1117e66b2614f41b3029510e37
Daniel Astine <daniel@astine.com.br>
2019-08-08 09:21:23 -0300

    Create Environment struct

---
+++ Movies/Movies/Common/Environment/Environment.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+enum Environment: String {
+    case development = "Development"
+    case production = "Production"
+    case staging = "Staging"
+}
+
+extension Environment {
+    static var current: Environment {
+        return from(Bundle.main)
+    }
+
+    static func from(_ projectInfo: ProjectInfo) -> Environment {
+        return projectInfo.environment
+    }
+}
+
+extension ProjectInfo {
+    var environment: Environment {
+        let environmentNameKey = "Environment"
+
+        guard let environmentName = infos?[environmentNameKey] as? String,
+            let environment = Environment(rawValue: environmentName) else {
+                return .development
+        }
+
+        return environment
+    }
+}
