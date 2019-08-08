a19fd0bee04a69395a6d2af365094b7f187f9e0f
Daniel Astine <daniel@astine.com.br>
2019-08-08 15:38:01 -0300

    Create Genres API resource

---
+++ Movies/Movies/Core/APIService/Services/Genres/GenresResource.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+struct GenresResource {
+    // MARK: - Properties
+    private (set) var baseURL: URLConvertible
+    private (set) var version: String?
+    private (set) var method: HTTPMethod
+    private (set) var endpoint: Endpoint
+    private (set) var parser: ResourceParser
+}
+
+// MARK: - Resource extension
+extension GenresResource: Resource { }
+
+// MARK: - Methods
+extension GenresResource {
+    static func genres(info: APIServiceInfoProtocol = APIServiceInfo.default) -> GenresResource {
+        let queryItems: [URLQueryItem] = [.apiKey(value: info.apiKey)]
+        let endpoint = Endpoint(path: "genre/movie/list",
+                                queryItems: queryItems)
+
+        let parser = ClosureResourceParser(GenresResource.parser)
+
+        return GenresResource(baseURL: info.baseURL,
+                              version: info.version,
+                              endpoint: endpoint,
+                              parser: parser)
+    }
+}
+
+// MARK: - Private extension
+private extension GenresResource {
+    // MARK: - Initialization
+    init(baseURL: URLConvertible,
+         version: String? = nil,
+         endpoint: Endpoint,
+         method: HTTPMethod = .get,
+         parser: ResourceParser) {
+        self.baseURL = baseURL
+        self.version = version
+        self.endpoint = endpoint
+        self.method = method
+        self.parser = parser
+    }
+
+    static func parser(data: Data?) throws -> ResponseModel.Genres {
+        guard let data = data else {
+            return ResponseModel.Genres(genres: [])
+        }
+
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+        return try decoder.decode(ResponseModel.Genres.self, from: data)
+    }
+}
